7340f3c8a2dab7625134574d3b3e9802c9147c32
marzece <marzece@gmail.com>
2021-03-17 15:34:50 -0400

    add data pipeline interface

---
+++ fakernet_code/data_pipeline.c
@@ -0,0 +1,56 @@
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "data_pipeline.h"
+
+
+uint32_t read_addr(uint32_t, uint32_t);
+uint32_t double_read_addr(uint32_t, uint32_t);
+int write_addr(uint32_t, uint32_t, uint32_t);
+
+#define THRESHOLD_REG_OFFSET 0x0
+#define CHANNEL_MASK_OFFSET 0x4
+#define DEPTH_ADDR_OFFSET 0x8
+#define DEPTH_ADDR_WIDTH 0x4
+
+AXI_DATA_PIPELINE* new_data_pipeline_if(const char* name, uint32_t axi_addr) {
+    AXI_DATA_PIPELINE* ret = malloc(sizeof(AXI_DATA_PIPELINE));
+    ret->name = name;
+    ret->axi_addr = axi_addr;
+    return ret;
+}
+
+uint32_t read_data_pipeline_value(AXI_DATA_PIPELINE *dp_axi, uint32_t offset) {
+    return double_read_addr(dp_axi->axi_addr, offset);
+}
+
+uint32_t write_data_pipeline_value(AXI_DATA_PIPELINE* dp_axi, uint32_t offset, uint32_t data) {
+    return write_addr(dp_axi->axi_addr, offset, data);
+}
+
+uint32_t read_threshold(AXI_DATA_PIPELINE* dp_axi) {
+    return read_data_pipeline_value(dp_axi, THRESHOLD_REG_OFFSET);
+}
+
+uint32_t write_threshold(AXI_DATA_PIPELINE* dp_axi, uint32_t data) {
+    return write_data_pipeline_value(dp_axi, THRESHOLD_REG_OFFSET, data);
+}
+
+uint32_t read_channel_mask(AXI_DATA_PIPELINE* dp_axi) {
+    return read_data_pipeline_value(dp_axi, CHANNEL_MASK_OFFSET);
+
+}
+
+uint32_t write_channel_mask(AXI_DATA_PIPELINE* dp_axi, uint32_t mask) {
+    return write_data_pipeline_value(dp_axi, CHANNEL_MASK_OFFSET, mask);
+}
+
+uint32_t write_channel_depth(AXI_DATA_PIPELINE* dp_axi, int channel, uint32_t val) {
+    uint32_t offset = DEPTH_ADDR_OFFSET + DEPTH_ADDR_WIDTH*channel;
+    return write_data_pipeline_value(dp_axi, offset, val);
+}
+
+uint32_t read_channel_depth(AXI_DATA_PIPELINE* dp_axi, int channel) {
+    uint32_t offset = DEPTH_ADDR_OFFSET + DEPTH_ADDR_WIDTH*channel;
+    return read_data_pipeline_value(dp_axi, offset);
+}
---
+++ fakernet_code/data_pipeline.h
@@ -0,0 +1,20 @@
+#ifndef __DATA_PIPELINE__
+#define __DATA_PIPELINE__
+#include <inttypes.h>
+#include <stdlib.h>
+
+typedef struct AXI_DATA_PIPELINE {
+    const char* name;
+    uint32_t axi_addr;
+} AXI_DATA_PIPELINE;
+
+AXI_DATA_PIPELINE* new_data_pipeline_if(const char* name, uint32_t axi_addr);
+uint32_t read_data_pipeline_value(AXI_DATA_PIPELINE *dp_axi, uint32_t offset);
+uint32_t write_data_pipeline_value(AXI_DATA_PIPELINE* dp_axi, uint32_t offset, uint32_t data);
+uint32_t read_threshold(AXI_DATA_PIPELINE* dp_axi);
+uint32_t write_threshold(AXI_DATA_PIPELINE* dp_axi, uint32_t data) ;
+uint32_t read_channel_depth(AXI_DATA_PIPELINE* dp_axi, int channel);
+uint32_t write_channel_depth(AXI_DATA_PIPELINE* dp_axi, int channel, uint32_t val);
+uint32_t write_channel_mask(AXI_DATA_PIPELINE* dp_axi, uint32_t mask);
+uint32_t read_channel_mask(AXI_DATA_PIPELINE* dp_axi);
+#endif
