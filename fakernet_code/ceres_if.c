c2e36460cfbce266073317d10a28f0a58927e9d1
marzece <marzece@gmail.com>
2021-07-12 16:55:22 -0400

    update CDCE6214 IIC Address in CERES interface

--- fakernet_code/ceres_if.c
+++ fakernet_code/ceres_if.c
@@ -31,8 +31,7 @@
 
 #define  DATA_PIPELINE_0_ADDR    0x6000
 
-//#define CLKGEN_IIC_ADDR         0xD2
-#define CLKGEN_IIC_ADDR          0xD0
+#define CLKGEN_IIC_ADDR          0xCE
 
 const uint32_t CERES_SAFE_READ_ADDRESS = GPIO0_AXI_ADDR;
 
