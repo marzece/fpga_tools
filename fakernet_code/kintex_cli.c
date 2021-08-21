a17a83134824b7be69ebc1187f013bd56c25888f
marzece <marzece@gmail.com>
2021-08-21 13:40:58 -0400

    get CLI to work with server

--- fakernet_code/kintex_cli.c
+++ fakernet_code/kintex_cli.c
@@ -4,16 +4,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <arpa/inet.h>
 #include <linenoise.h>
 
 #define BUFFER_SIZE 16384
-#define SEND_COMMAND_TABLE_COMMAND "send_command_table"
+#define SEND_COMMAND_TABLE_COMMAND "send_command_table\r\n"
 
 FILE* debug_file;
 char* fpga_cli_hint_str = NULL;
 
-static int command_fd = 0;
-static int response_fd = 0;
 char response_buffer[BUFFER_SIZE];
 void linenoiseSetFreeHintsCallback(linenoiseFreeHintsCallback *fn);
 
@@ -23,8 +22,8 @@ typedef struct ServerCommand {
 } ServerCommand;
 static ServerCommand* commandTable = NULL;
 
-int grab_response(void) {
-    int nbytes_read = read(response_fd, response_buffer, BUFFER_SIZE);
+int grab_response(int fd) {
+    int nbytes_read = read(fd, response_buffer, BUFFER_SIZE);
     if(nbytes_read >= BUFFER_SIZE-1) {
         printf("Response too long...can't handle this\n");
         exit(1);
@@ -132,17 +131,19 @@ CLEAR_COMMAND_TABLE:;
     commandTable = NULL;
 }
 
-int handle_line(const char* line) {
+int handle_line(const int fd, const char* line) {
     // first check if the first char is a '#' or the line is empty
     // if it is, treat this as a comment
     if(strlen(line) == 0 || line[0] == '#') {
         return 0;
     }
+
     // Write the given line (without NULL terminator)
-    write(command_fd, line, strlen(line));
+    write(fd, line, strlen(line));
+    write(fd, "\r\n", 2); // TODO this should be only added if needed!
 
     // Fills result into response_buffer
-    int nbytes_read = grab_response();
+    int nbytes_read = grab_response(fd);
 
     if(response_buffer[nbytes_read-1] != '\n') {
         printf("%s\n", response_buffer);
@@ -151,6 +152,88 @@ int handle_line(const char* line) {
         printf("%s", response_buffer);
     }
     return 0;
+}
+
+int connect_to_server(const char* ip, const int port) {
+    int args;
+    int res;
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_port = htons(port);
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(fd < 0) {
+        printf("Error creating TCP socket: %s\n", strerror(errno));
+        return fd;
+    }
+
+    // Set the socket to non-block before connecting
+    args  = fcntl(fd, F_GETFL, NULL);
+    if(args < 0) {
+        printf("Error getting socket opts\n");
+        goto error;
+
+    }
+    //args |= O_NONBLOCK;
+
+    // Idk if this is needed!
+    //int yes = 1;
+    //setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+
+    if(fcntl(fd, F_SETFL, args) < 0) {
+        printf("Error setting socket opts\n");
+        goto error;
+    }
+    fd_set myset;
+
+    while(1) {
+        res = connect(fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+        if(res < 0) {
+            if(errno == EISCONN) {
+                break;
+            }
+            else if (errno == EINPROGRESS) {
+                struct timeval tv;
+                tv.tv_sec = 15;
+                tv.tv_usec = 0;
+                FD_ZERO(&myset);
+                FD_SET(fd, &myset);
+                res = 0;
+                res = select(fd+1, NULL, &myset, NULL, &tv);
+                if(res != 1) {
+                    goto error;
+                }
+                break;
+            }
+            printf("Error connecting TCP socket: %s\n", strerror(errno));
+            sleep(5);
+            continue;
+        }
+        break;
+    }
+
+    // Set it back to blocking mode
+    // Idk if it's necessary to get args again, but that's how it's done on stack overflow
+    args  = fcntl(fd, F_GETFL, NULL);
+    if(args < 0) {
+        printf("Error getting socket opts2\n");
+        goto error;
+
+    }
+    args &= (~O_NONBLOCK);
+
+    if(fcntl(fd, F_SETFL, args) < 0) {
+        printf("Error setting socket opts2\n");
+        goto error;
+
+    }
+
+    return fd;
+
+error:
+    close(fd);
+    return -1;
 }
 
 int main(int argc, char **argv) {
@@ -188,25 +271,20 @@ int main(int argc, char **argv) {
     linenoiseHistoryLoad("history.txt"); /* Load the history at startup */
     const char* prompt_string = "kintex> ";
 
-    const char* command_fn = "kintex_command_pipe";
-    const char* resp_fn = "kintex_response_pipe";
+    const char* server_ip = "127.0.0.1"; // Localhost
+    const int server_port = 4002;
+    int server_fd = connect_to_server(server_ip, server_port);
 
-    command_fd = open(command_fn, O_WRONLY);
-    if(command_fd <= 0) {
+    if(server_fd <= 0) {
         printf("Error ocurred opening command pipe\n");
         printf("%s\n", strerror(errno));
-        return 1;
-    }
-    response_fd = open(resp_fn, O_RDONLY);
-    if(response_fd <= 0) {
-        printf("Error ocurred opening response pipe\n");
         return 1;
     }
 
     // First get the command table
     if(!batch) {
-        write(command_fd, SEND_COMMAND_TABLE_COMMAND, strlen(SEND_COMMAND_TABLE_COMMAND));
-        grab_response();
+        write(server_fd, SEND_COMMAND_TABLE_COMMAND, strlen(SEND_COMMAND_TABLE_COMMAND));
+        grab_response(server_fd);
         produce_command_table(response_buffer);
     }
 
@@ -219,7 +297,7 @@ int main(int argc, char **argv) {
     while((line = linenoise(prompt_string)) != NULL) {
         /* Do something with the string. */
         if (line[0] != '\0' && line[0] != '/') {
-            handle_line(line);
+            handle_line(server_fd, line);
             linenoiseHistoryAdd(line); /* Add to the history. */
             linenoiseHistorySave("history.txt"); /* Save the history on disk. */
         } else if (!strncmp(line,"/historylen",11)) {
