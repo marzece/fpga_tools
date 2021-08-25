a72763efe1ed4179dd668cb2e0ee2fc629f5a2d5
marzece <marzece@gmail.com>
2021-08-25 12:09:42 -0400

    removed fogotten use of redis's memory alloc system

--- fakernet_code/ae_epoll.c
+++ fakernet_code/ae_epoll.c
@@ -59,7 +59,7 @@ static int aeApiCreate(aeEventLoop *eventLoop) {
 static int aeApiResize(aeEventLoop *eventLoop, int setsize) {
     aeApiState *state = eventLoop->apidata;
 
-    state->events = zrealloc(state->events, sizeof(struct epoll_event)*setsize);
+    state->events = realloc(state->events, sizeof(struct epoll_event)*setsize);
     return 0;
 }
 
