b329641cbd9f33e7d904efd072a5bf8be5793786
marzece <marzece@gmail.com>
2021-09-16 16:31:51 -0400

    add error/sync rate polling scripts

--- poll_sync_rate.py
+++ poll_sync_rate.py
@@ -7,15 +7,11 @@ from collections import namedtuple, defaultdict
 
 if __name__ == "__main__":
     import argparse
-    fout = open("poll_result.dat", 'w', buffering=1)
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", default="localhost", type=str, help="IP of Kintex command server")
     parser.add_argument("--port", default=4002, type=int, help="Port of Kintex command server")
-    parser.add_argument("--jesds", default=[0,1,2,3], nargs='+',  help="Which JESD streams should be monitored")
     args = parser.parse_args()
     fd = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    print(args)
-    jesds = [int(x) for x in args.jesds]
     try:
         fd.connect((args.ip, args.port))
     except OSError:
@@ -31,32 +27,33 @@ if __name__ == "__main__":
     prev_data = defaultdict(list)
     while(True):
         data = defaultdict(list)
-        for lane in jesds:
+        for channel in [0, 1]:
             t = time()
-            fd.sendall(b"jesd_error_count %i\r\n" % (int(lane),))
+            fd.sendall(b"read_gpio1 %i\r\n" % (int(channel),))
             reader.feed(fd.recv(1024)) # !TODO add time out to this
             result = reader.gets()
             #result = np.random.randint(low=0, high=500, size=4)
-            print(t, lane, result)
-            fout.write("%s %s %s\n" % (str(t), str(lane), str(result)))
+            #print(t, channel, result)
             if type(result) == hiredis.ReplyError:
                 print("Error response from server: %s" % str(result))
                 continue
 
-            data[lane] = Result(t, result)
-            if(prev_data and prev_data[lane]):
-                dt = data[lane].time - prev_data[lane].time
+            data[channel] = Result(t, result)
+            if(prev_data and prev_data[channel]):
+                dt = data[channel].time - prev_data[channel].time
                 if(dt <= 0):
                     # HMMM how the heck did this happen
                     print("negative or zero time found....not sure what happened")
                     continue
-                dv = [(v - prev_v)/dt for v, prev_v in zip(data[lane].vals, prev_data[lane].vals)]
-                if(any([x < 0 for x in dv])):
+                dv = (data[channel].vals - prev_data[channel].vals)/dt
+                dv /= 250e6
+                if(dv < 0):
                     # You'll get negative values if the JESD core was reset
                     print("negative error count found...probably a reset")
-                    prev_data[lane] = data[lane]
+                    prev_data[channel] = data[channel]
                     continue
                 else:
-                    redis_db.publish("jesd_errors", "{} {} {} {} {}".format(lane, *dv))
-            prev_data[lane] = data[lane]
+                    #redis_db.publish("jesd_sync", "{} {} {}".format(channel, *dv))
+                    print("jesd_sync", "{} {}".format(channel, round(dv, 3)))
+            prev_data[channel] = data[channel]
         sleep(2)
